3929fc71a8a7efd669b72364335a657441514ea4
Arcady1 <insite1029@gmail.com>
2020-07-24 18:22:39 +0300

    tf-expandDims

--- js/modules.js
+++ js/modules.js
@@ -1,23 +1,29 @@
-// img = numjs.expand_dims(img, axis = 0) // [64, 64, 1 ] -> [1, 64, 64, 1]
-// Create model const model = await tf.loadLayersModel('<path to json file>');
 // Predict const prediction = model.predict(img); [1, 340] // flatten
 // const index = numpyjs.argmax(prediction)[1]
 // conds class_name = name_list[index]
 
+// modules 
+const tf = require('@tensorflow/tfjs');
+// vars
 let canv2 = document.getElementById('canv2'); // canvas-resizer (64x64)
 let ctx2 = canv2.getContext('2d');
 let imgSize = 64; // размер canv2
 // ф-ия для получения изображения с canvas
 function getImage(canvas) {
+    // * preparing image
     let img2Data;
-
     ctx2.drawImage(canvas, 0, 0, imgSize, imgSize); // рисует изображение с canvas в canv2
     img2Data = ctx2.getImageData(0, 0, imgSize, imgSize); // массив чисел 0 - 255 - изображение canv2
     img2Data = img2Data.data; // оставляем в img2Data просто массив чисел 
-    // ! массив размером 16.384, т.к. каждый пиксель в rgba формате => на один пиксель - 4 элемента => 64*64*4 = 16.384
+    // массив размером 16.384, т.к. каждый пиксель в rgba формате => на один пиксель - 4 элемента => 64*64*4 = 16.384
     img2Data = rgbaOnlyAImg(img2Data);
     img2Data = bwImgColor(img2Data); // img2Data - массив из 64*64 = 4096 элементов (черно-белая картинка)
     console.log(img2Data);
+    // * tf work
+    let img = tf.expandDims(img2Data, 0);
+    console.log(img);
+    // ! const model = await tf.loadLayersModel('../model/model.json');
+    // ! const prediction = model.predict(img);
 }
 // очистка canv2
 function cleanCanv2() {
@@ -38,6 +44,7 @@ function bwImgColor(imgData) {
             imgData[i] = 255;
     return imgData;
 }
+
 
 module.exports = {
     "getImage": getImage,
