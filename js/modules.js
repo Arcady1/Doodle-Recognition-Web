dbccabab135950c8d7507fe10555616ee02be4d7
Arcady1 <insite1029@gmail.com>
2020-07-24 16:57:58 +0300

    64x64-bw-format

--- js/modules.js
+++ js/modules.js
@@ -1,5 +1,3 @@
-// изменяю размер (64х64) // img = cv2.resize(img, (64, 64, 3))
-// делаю черно-белой (https://stackoverflow.com/questions/42099769/opencv-color-bgr2gray-error) //[64, 64, 3] -> [64, 64, 1]
 // img = numjs.expand_dims(img, axis = 0) // [64, 64, 1 ] -> [1, 64, 64, 1]
 // Create model const model = await tf.loadLayersModel('<path to json file>');
 // Predict const prediction = model.predict(img); [1, 340] // flatten
