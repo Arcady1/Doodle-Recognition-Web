5bce5f000afb00ad3935e4499526d86ddf4a14d0
Arcady1 <insite1029@gmail.com>
2020-07-27 16:23:00 +0300

    predict-array & cat-list

--- js/main.js
+++ js/main.js
@@ -9,8 +9,23 @@ const imgSize = 64; // размер canv2
 const loadedModel = tfModelWork.tfModelLoad(); // tf Model
 
 function main(canvas) {
+    let categories_list;
     let img = imgPreparing.getImage(canvas, imgSize); // prepared img -> [1, 64, 64, 1]
-    tfModelWork.tfPredict(loadedModel, img);
+    let maxPredictValue = tfModelWork.tfPredict(loadedModel, img); // predict
+    // getting categories_list from JSON -> typeof(categories_list) - array of categories [340]
+    let categories_listGetter = new Promise((resolve, reject) => {
+        $.getJSON('../model/categories_list.json', function (res) {
+            categories_list = res;
+            resolve();
+        });
+    });
+    // ! results
+    categories_listGetter.then(() => {
+        console.log(categories_list);
+    });
+    maxPredictValue.then((res) => {
+        console.log(res);
+    })
 }
 
 module.exports = {
--- js/prj-modules/tf-model-work.js
+++ js/prj-modules/tf-model-work.js
@@ -2,21 +2,39 @@
 const tf = require('@tensorflow/tfjs');
 const math = require('mathjs');
 
-// Model loading
+// Model loading; function returns Promise
 async function tfModelLoad() {
     const model = await tf.loadLayersModel('../model/mod1/model.json');
     console.log('The Model was loaded!');
     return model;
 }
-// Predict
-function tfPredict(loadedModel, img) {
-    let prediction = loadedModel.then((model) => {
-        let predictionResults = model.predict(img).arraySync(); // prediction Results
-        console.log(predictionResults);
-        
-        let maxPredictValue = math.max(predictionResults);
-        console.log(maxPredictValue);
+// Predict; function returns Promise
+async function tfPredict(loadedModel, img) {
+    let maxPredictValue = await loadedModel.then((model) => {
+        let predictionResults = model.predict(img).arraySync()[0]; // prediction Results -> array
+        return maxPredict(predictionResults); // prediction maxValue and maxIndex
     });
+
+    return maxPredictValue;
+}
+// maxValue in predictionResultsArray and maxIndex; returns maxPredictionResults - object
+function maxPredict(predictionResults) {
+    let maxPredictionResults = {
+        "maxVal": 0,
+        "maxValIndex": 0
+    };
+
+    for (let i = 0; i < predictionResults.length - 1; i++) {
+        if (predictionResults[i] > predictionResults[i + 1]) {
+            maxPredictionResults.maxVal = predictionResults[i];
+            maxPredictionResults.maxValIndex = i;
+        } else {
+            maxPredictionResults.maxVal = predictionResults[i + 1];
+            maxPredictionResults.maxValIndex = i + 1;
+        }
+    }
+
+    return maxPredictionResults;
 }
 
 module.exports = {
